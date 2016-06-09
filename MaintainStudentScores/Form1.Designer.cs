067da658c98482140ad1924195a9ea229e1f6f4d
truonart <arthurtruong22@gmail.com>
2016-06-09 13:18:05 -0700

    Add files via upload

---
+++ MaintainStudentScores/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace MaintainStudentScores
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStudents = new System.Windows.Forms.Label();
+            this.lstStudents = new System.Windows.Forms.ListBox();
+            this.lblScoreTotal = new System.Windows.Forms.Label();
+            this.lblScoreCount = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtScoreTotal = new System.Windows.Forms.TextBox();
+            this.txtAverage = new System.Windows.Forms.TextBox();
+            this.txtScoreCount = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblStudents
+            // 
+            this.lblStudents.AutoSize = true;
+            this.lblStudents.Location = new System.Drawing.Point(24, 27);
+            this.lblStudents.Name = "lblStudents";
+            this.lblStudents.Size = new System.Drawing.Size(49, 13);
+            this.lblStudents.TabIndex = 0;
+            this.lblStudents.Text = "Students";
+            // 
+            // lstStudents
+            // 
+            this.lstStudents.FormattingEnabled = true;
+            this.lstStudents.Location = new System.Drawing.Point(27, 54);
+            this.lstStudents.Name = "lstStudents";
+            this.lstStudents.Size = new System.Drawing.Size(229, 95);
+            this.lstStudents.TabIndex = 1;
+            // 
+            // lblScoreTotal
+            // 
+            this.lblScoreTotal.AutoSize = true;
+            this.lblScoreTotal.Location = new System.Drawing.Point(179, 161);
+            this.lblScoreTotal.Name = "lblScoreTotal";
+            this.lblScoreTotal.Size = new System.Drawing.Size(62, 13);
+            this.lblScoreTotal.TabIndex = 2;
+            this.lblScoreTotal.Text = "Score Total";
+            // 
+            // lblScoreCount
+            // 
+            this.lblScoreCount.AutoSize = true;
+            this.lblScoreCount.Location = new System.Drawing.Point(179, 186);
+            this.lblScoreCount.Name = "lblScoreCount";
+            this.lblScoreCount.Size = new System.Drawing.Size(66, 13);
+            this.lblScoreCount.TabIndex = 3;
+            this.lblScoreCount.Text = "Score Count";
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Location = new System.Drawing.Point(194, 214);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(47, 13);
+            this.lblAverage.TabIndex = 4;
+            this.lblAverage.Text = "Average";
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Location = new System.Drawing.Point(273, 27);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(75, 23);
+            this.btnAddNew.TabIndex = 5;
+            this.btnAddNew.Text = "Add New...";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(273, 72);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(273, 110);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(277, 251);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // txtScoreTotal
+            // 
+            this.txtScoreTotal.Location = new System.Drawing.Point(257, 161);
+            this.txtScoreTotal.Name = "txtScoreTotal";
+            this.txtScoreTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtScoreTotal.TabIndex = 9;
+            // 
+            // txtAverage
+            // 
+            this.txtAverage.Location = new System.Drawing.Point(257, 211);
+            this.txtAverage.Name = "txtAverage";
+            this.txtAverage.Size = new System.Drawing.Size(100, 20);
+            this.txtAverage.TabIndex = 11;
+            // 
+            // txtScoreCount
+            // 
+            this.txtScoreCount.Location = new System.Drawing.Point(257, 186);
+            this.txtScoreCount.Name = "txtScoreCount";
+            this.txtScoreCount.Size = new System.Drawing.Size(100, 20);
+            this.txtScoreCount.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 283);
+            this.Controls.Add(this.txtScoreCount);
+            this.Controls.Add(this.txtAverage);
+            this.Controls.Add(this.txtScoreTotal);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAddNew);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.lblScoreCount);
+            this.Controls.Add(this.lblScoreTotal);
+            this.Controls.Add(this.lstStudents);
+            this.Controls.Add(this.lblStudents);
+            this.Name = "Form1";
+            this.Text = "Student Scores";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStudents;
+        private System.Windows.Forms.ListBox lstStudents;
+        private System.Windows.Forms.Label lblScoreTotal;
+        private System.Windows.Forms.Label lblScoreCount;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.Button btnAddNew;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtScoreTotal;
+        private System.Windows.Forms.TextBox txtAverage;
+        private System.Windows.Forms.TextBox txtScoreCount;
+    }
+}
+
